a8667a060c7e82d01614bd0aee00a74669838236
= <noams250@gmail.com>
2025-01-20 16:42:36 +0200

    modify file system

--- python/mod/file.py
+++ python/mod/file.py
@@ -1,63 +1,105 @@
 import sys
 # from typing import TextIO
 
-def writeImport(file: str, lines) -> None: # file format 'w'
-    foundSimilarImport = False # no need, but good convention wise
+def writeImport(file: str, lines, importLocation: str, importModule: str, entity: str) -> None: # file format 'w'
+    foundSimilarImport = False # no need, but good convention wise    
+    
+    for line_number, line in enumerate(lines):
+        if f"from {importLocation}.{importModule}" in line and entity in line and line.strip()[0] != '#':
+            return;
+
     for line_number, line in enumerate(lines): # specfic import
         
-        if "import ..." in line:
+        if f"from {importLocation}.{importModule}" in line and line.strip()[0] != '#':
             # add import to file itself here
-            lines[line_number] = line.strip() + "import module ...\n"
+            # adds entity to same import module
+            lines[line_number] = line.strip() + f", {entity}\n"
             foundSimilarImport = True
             break
     
     if foundSimilarImport:
         with open(file, 'w') as f:
-            f.write(lines)
+            f.writelines(lines)
             f.close()
         return
     
     for line_number, line in enumerate(lines): # less specific import
-        if "less specific import..." in line:
+        if f"from {importLocation}" in line:
             # add import to line itself here
-            lines[line_number] = line + "new import\n"
+            # makes sure all rest framework imports are bundled together
+            lines.insert(line_number, f"from {importLocation}.{importModule} import {entity}\n")
             foundSimilarImport = True
             break
     
     if foundSimilarImport:
         with open(file, 'w') as f:
-            f.write(lines)
+            f.writelines(lines)
             f.close()
         return
     
-    lines.insert(0, "import\n")
+    # if not any rest framework imports, just add it to first line of code
+    lines.insert(0, f"from {importLocation}.{importModule} import {entity}\n")
 
-def writeScope(file: str, scope: str, inClass: str, lines) -> None:
+    with open(file, 'w') as f:
+        f.writelines(lines)
+        f.close()
+
+def writeClassScope(file: str, scope: str, inClass: str, lines) -> None:
     for line_number, line in enumerate(lines):
         if f"class {inClass}" in line:
-            lines[line_number + 1] = "scope...\n"
-            lines[line_number + 2] = "scope_list..\n"
+            lines.insert(line_number + 1, f"    throttle_scope = \"{scope}\"\n")
+            lines.insert(line_number + 2, "    throttle_classes = [ScopedRateThrottle]\n")
             break
     
     with open(file, 'w') as f:
-        f.write(lines)
+        f.writelines(lines)
         f.close()
 
-def write(file: str, scope: str, inClass: str, lines: list) -> None:
+def writeFunctionScope(filePath: str, scope: str, function: str, lines: list):
     
-    writeImport(file=file, lines=lines)
-    writeScope(file=file, scope=scope, inClass=inClass, lines=lines)
+    line_number = 0;
+    for line_number, line in enumerate(lines):
+        if f"def {function}" in line:
+            lines.insert(line_number, f"@rate_limit_scope(\"{scope}\")\n")
+            break
 
-if __name__ == "__file__":
-    arguments = sys.argv[1:] # exclude script path
-    #argv: [type, filePath, scope, class]
-    fileLines = []
-    with open(arguments[1], 'r') as file:
-        fileLines = file.readlines()
+    with open(filePath, 'w') as file:
+        file.writelines(lines)
         file.close()
 
-    match arguments[0]:
-        case "writeScope":
-            write(arguments[1], arguments[2], arguments[3], fileLines)
-        case "readFile":
-            print(fileLines)
+
+def write(file: str, scope: str, viewName: str, lines: list) -> None:
+    
+    viewType = ""
+    for line in lines:
+        if f"class {viewName}" in line:
+            viewType = "class"
+            break
+        elif f"def {viewName}" in line:
+            viewType = "function"
+            break
+
+    if viewType == "": return
+
+    match viewType:
+        case "class":
+            writeImport(file, lines, "rest_framework", "throttling", "UserRateThrottle")
+            writeClassScope(file, scope, viewName, lines)
+        case "function":
+            writeImport(file, lines, "middleware", "rate_limit", "rate_limit_scope")
+            writeFunctionScope(file, scope, viewName, lines)
+
+
+arguments = sys.argv[1:] # exclude script path
+#arguments: [type, filePath, scope, viewName]
+fileLines = []
+with open(arguments[1], 'r') as file:
+    fileLines = file.readlines()
+    file.close()
+
+match arguments[0]:
+    case "writeScope":
+        write(arguments[1], arguments[2], arguments[3], fileLines)
+        # print(fileLines)
+    case "readFile":
+        print(fileLines)
--- python/mod/tojson.py
+++ python/mod/tojson.py
@@ -4,19 +4,21 @@ def toJson(relativePath: str, outputPath: str) -> list:
     info = []
     
     with open(relativePath, 'r') as file:
-        line = file.readline()
-        while line:
-            if line[0] == '#':
-                info.append({
-                    "path": line[2:-1],
-                    "status": False,
-                    "scope": None,
-                    "why": None,
-                })
-            line = file.readline()
+        lines = file.readlines()
         file.close()
+    
+    for line in lines:
+        if line[0] == '#':
+            info.append({
+                "path": line[2:-1],
+                "status": False,
+                "scope": None,
+                "why": None,
+            })
+    
 
     json = jsonify(info)
     with open(outputPath, 'w') as outputFile:
         outputFile.write(json)
+        outputFile.close()
     return info # returns list! not json
